e59943ec24ae07c4c389fdd08bc0a766d1ef5b4c
Mary Flagstad <mflagstad@cj.com>
2016-06-06 21:28:22 -0700

    split data into training and test, pull value matrix functionality out of main

--- drinking.py
+++ drinking.py
@@ -1,10 +1,19 @@
 import numpy
 import json
+import random
 from collections import namedtuple
 
 def load_json_file(file_path):
   f = open(file_path)
   return json.load(f)
+
+def split_train_test(data, train_percent):
+    m = len(data)
+    num_to_select = int(train_percent * m)
+    training = data[0:num_to_select]
+    test = data[num_to_select:m]
+    return training, test
+
 
 def main():
   raw = load_json_file('./students.json')
@@ -14,14 +23,18 @@ def main():
   # 6 parent status
   # 28 weekend alcohol (Y value)
   value_indices = [1, 3, 4, 5, 27]
- 
+  values = value_matrix(raw, value_indices)
+  train, test = split_train_test(values, .7)
+  print len(train), len(test)
+
+def value_matrix(raw, value_indices):
   matrix = []
   for item in raw['data']:
     row = []
     for idx in value_indices:
       row.append(item['values'][idx])
     matrix.append(row)
-  print matrix
+  return matrix
 
 if __name__ == '__main__':
   main()
