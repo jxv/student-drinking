18c334c95055133fb30cd08483f0aa1fb474f48d
jvargas <jvargas@cj.com>
2016-06-06 21:02:36 -0700

    put json data into matrix

--- drinking.py
+++ drinking.py
@@ -7,9 +7,21 @@ def load_json_file(file_path):
   return json.load(f)
 
 def main():
-  print 'hello, world!'
-  some_json = load_json_file('./students.json')
-  print some_json
+  raw = load_json_file('./students.json')
+  # 2 sex
+  # 4 address
+  # 5 family size
+  # 6 parent status
+  # 28 weekend alcohol (Y value)
+  value_indices = [1, 3, 4, 5, 27]
+ 
+  matrix = []
+  for item in raw['data']:
+    row = []
+    for idx in value_indices:
+      row.append(item['values'][idx])
+    matrix.append(row)
+  print matrix
 
 if __name__ == '__main__':
   main()
