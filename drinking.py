4235044118f31a0976555b82016ac20932e32879
jvargas <jvargas@cj.com>
2016-06-07 09:39:58 -0700

    create mapping between value's raw index and its cat_to_num xformers

--- drinking.py
+++ drinking.py
@@ -2,6 +2,7 @@ import numpy
 import json
 import random
 from collections import namedtuple
+from functools import partial
 
 def load_json_file(file_path):
   f = open(file_path)
@@ -14,27 +15,25 @@ def split_train_test(data, train_percent):
     test = data[num_to_select:m]
     return training, test
 
-def cat_to_num(row):
-    return [
-      1 if row[0] == 'M' else 0,
-      1 if row[1] == 'U' else 0,
-      1 if row[2] == 'GT3' else 0,
-      1 if row[3] == 'T' else 0,
-      1 if row[4] == '4' or row[4] == '5' else 0
-    ]
-
+def cat_to_num(mapper, row):
+  mapper_rows = zip(mapper, row)
+  return map(lambda (xform, cat): xform(cat), mapper_rows)
 
 def main():
   raw = load_json_file('./students.json')
-  value_indices = [
-    1, # sex (M, F)
-    3, # address (R, U)
-    4, # family size (LT3, GT3)
-    5, # parent status (A, T)
-    27 # weekend alcohol (1-3, 4-5) (Y value)
+
+  value_map = [
+    (1, lambda sex: 1 if sex == 'M' else 0), # [M, F]
+    (3, lambda address: 1 if address == 'U' else 0), # [R, U]
+    (4, lambda family_size: 1 if family_size == 'GT3' else 0), # [LT3, GT3]
+    (5, lambda parent_status: 1 if parent_status == 'T' else 0), # [A, T]
+    (27, lambda weekend_alcohol: 1 if weekend_alcohol == '4' or weekend_alcohol == '5' else 0), # [1-3, 4-5]
   ]
+  value_indices = map(lambda mapping: mapping[0], value_map)
+  value_mapper = map(lambda mapping: mapping[1], value_map)
+
   cat_values = value_matrix(raw, value_indices)
-  num_values = map(cat_to_num, cat_values)
+  num_values = map(partial(cat_to_num, value_mapper), cat_values)
   train, test = split_train_test(num_values, .7)
   print train[0]
 
