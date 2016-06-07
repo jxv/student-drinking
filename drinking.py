485a401da13623fa8853055c0300730980658f03
jvargas <jvargas@cj.com>
2016-06-06 21:52:54 -0700

    map cat_to_num over data

--- drinking.py
+++ drinking.py
@@ -14,7 +14,7 @@ def split_train_test(data, train_percent):
     test = data[num_to_select:m]
     return training, test
 
-def tag_to_value(row):
+def cat_to_num(row):
     return [
       1 if row[0] == 'M' else 0,
       1 if row[1] == 'U' else 0,
@@ -32,10 +32,10 @@ def main():
   # 6 parent status
   # 28 weekend alcohol (Y value)
   value_indices = [1, 3, 4, 5, 27]
-  values = value_matrix(raw, value_indices)
-  train, test = split_train_test(values, .7)
+  cat_values = value_matrix(raw, value_indices)
+  num_values = map(cat_to_num, cat_values)
+  train, test = split_train_test(num_values, .7)
   print train[0]
-  print tag_to_value(train[0])
 
 def value_matrix(raw, value_indices):
   matrix = []
