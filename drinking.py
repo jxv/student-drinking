39a452a64b41c44c728b0209eab6ab0a9a36875a
Mary Flagstad <mflagstad@cj.com>
2016-06-06 21:47:07 -0700

    transform row from categories to numerical values

--- drinking.py
+++ drinking.py
@@ -14,6 +14,15 @@ def split_train_test(data, train_percent):
     test = data[num_to_select:m]
     return training, test
 
+def tag_to_value(row):
+    return [
+      1 if row[0] == 'M' else 0,
+      1 if row[1] == 'U' else 0,
+      1 if row[2] == 'GT3' else 0,
+      1 if row[3] == 'T' else 0,
+      1 if row[4] == '4' or row[4] == '5' else 0
+    ]
+
 
 def main():
   raw = load_json_file('./students.json')
@@ -25,7 +34,8 @@ def main():
   value_indices = [1, 3, 4, 5, 27]
   values = value_matrix(raw, value_indices)
   train, test = split_train_test(values, .7)
-  print len(train), len(test)
+  print train[0]
+  print tag_to_value(train[0])
 
 def value_matrix(raw, value_indices):
   matrix = []
